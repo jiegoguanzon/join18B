57aae0a0f24784cd6061e8376de041ba64a24058
Jiego Guanzon <jboguanzon@gmail.com>
2019-01-24 22:33:28 +0800

    3rd commit.

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,21 @@ var currentVideo = document.getElementById("main-video");
 var choiceContainer = document.getElementById("choice-container");
 var timerBar = document.getElementById("timer-bar");
 
-choiceContainer.addEventListener("click", function () {
-    choiceContainer.style.height = "20%";
+currentVideo.addEventListener("timeupdate", function () {
+    if(currentVideo.duration - currentVideo.currentTime <= 12)
+        choiceContainer.style.transform = "translateY(0%)";
 });
 
-timerBar.addEventListener("click", function () {
-    timerBar.style.width = "0%";
+choiceContainer.addEventListener("transitionend", function () {
+    if(choiceContainer.style.transform == "translateY(0%)"){
+        timerBar.style.transition = "width 10s linear";
+        timerBar.style.width = "0%";
+    } else if(choiceContainer.style.transform == "translateY(100%)"){
+        timerBar.style.transition = "width 0s linear";
+        timerBar.style.width = "100%";
+    }
+});
+
+timerBar.addEventListener("transitionend", function () {
+    choiceContainer.style.transform = "translateY(100%)";
 });
