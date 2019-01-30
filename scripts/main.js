acf717de88281a0d931aad1f06cc7fdb4c412e04
Jiego Guanzon <jboguanzon@gmail.com>
2019-01-31 06:04:21 +0800

    Form almost done.

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ var choice2 = document.getElementById("choice2");
 var logo= document.getElementById("circuit-logo");
 var timerBar = document.getElementById("timer-bar");
 
-const excessVideoDuration = 15.5; // 15 + 0.5 
+const excessVideoDuration = 10.5; // 10 + 0.5 
 
 var introStage = 0;
 
@@ -87,17 +87,19 @@ videoContainer.addEventListener("transitionend", function (e) {
 currentVideo.addEventListener("ended", function () {
     switch(parseVideoName()){
         case "snoozeAlarm.mp4":
-        case "commute.mp4":
+        case "commuteDenimJacket.mp4":
+        case "commutePinkJacket.mp4":
             nextVidSource = "media/late.mp4";
         break;
-        case "roadLessTravelled.mp4":
-        case "roadMoreTravelled.mp4":
+        case "runPinkJacket.mp4":
+        case "runDenimJacket.mp4":
             nextVidSource = "media/exam.mp4";
         break;
         case "late.mp4":
             restart();
         break;
         case "exam.mp4":
+            currentVideo.pause();
             videoContainer.style.opacity = "0";
             formContainer.style.opacity = "1";
         break;
@@ -116,16 +118,6 @@ currentVideo.addEventListener("timeupdate", function () {
     }
 });
 
-/*
-choiceContainer.addEventListener("transitionstart", function (e) {
-    console.log("choiceContainerStart");
-    if(e.propertyName == "transform" && choiceBarHidden){
-        timerBar.style.transition = "width 2s linear";
-        timerBar.style.width = "100%";
-    }
-});
-*/
-
 choiceContainer.addEventListener("transitionend", function (e) {
     console.log("choiceContainerEnd");
     if(e.propertyName == "transform"){
@@ -133,7 +125,7 @@ choiceContainer.addEventListener("transitionend", function (e) {
         if(!choiceBarHidden){
             choice1.className = "clickable-choice";
             choice2.className = "clickable-choice";
-            timerBar.style.transition = "width 10s linear";
+            timerBar.style.transition = "width 5s linear";
             timerBar.style.width = "0%";
         } else {
             timerBar.style.transition = "width 0s linear";
@@ -184,50 +176,40 @@ function updateSelection() {
                 nextVidSource = "media/snoozeAlarm.mp4";
             } else if(currentChoice == 2){
                 showChoices = 1;
-                nextChoice1 = "Attire 1";
-                nextChoice2 = "Attire 2";
+                nextChoice1 = "Pink Jacket";
+                nextChoice2 = "Denim Jacket";
                 nextVidSource = "media/stopAlarm.mp4";
             }
         break;
 
         case "stopAlarm.mp4":
             if(currentChoice == 1){
-                nextVidSource = "media/poloShirt.mp4";
+                nextVidSource = "media/pinkJacket.mp4";
             } else if(currentChoice == 2){
-                nextVidSource = "media/tShirt.mp4";
+                nextVidSource = "media/denimJacket.mp4";
             }
             showChoices = 1;
-            nextChoice1 = "Walk";
+            nextChoice1 = "Run";
             nextChoice2 = "Commute";
         break;
 
-        case "poloShirt.mp4":
-        case "tShirt.mp4":
+        case "denimJacket.mp4":
             if(currentChoice == 1){
-                showChoices = 1;
-                nextChoice1 = "Path 1";
-                nextChoice2 = "Path 2";
-                nextVidSource = "media/walk.mp4"
+                nextVidSource = "media/runDenimJacket.mp4"
             } else if(currentChoice == 2){
-                showChoices = 0;
-                nextVidSource = "media/commute.mp4"
-            }
-        break;
-
-        case "walk.mp4":
-            if(currentChoice == 1){
-                nextVidSource = "media/roadLessTravelled.mp4"
-            } else if(currentChoice == 2){
-                nextVidSource = "media/roadMoreTravelled.mp4"
+                nextVidSource = "media/commuteDenimJacket.mp4"
             }
             showChoices = 0;
         break;
 
-        /*
-        case "roadLessTravelled.mp4":
-        case "roadMoreTravelled.mp4":
+        case "pinkJacket.mp4":
+            if(currentChoice == 1){
+                nextVidSource = "media/runPinkJacket.mp4"
+            } else if(currentChoice == 2){
+                nextVidSource = "media/commutePinkJacket.mp4"
+            }
+            showChoices = 0;
         break;
-        */
 
     }
 }
